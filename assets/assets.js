eeffce65533d86f87295a85a1a354a92fd3f0721
Samuel David Peterson <s3722250@student.rmit.edu.au>
2025-06-21 14:49:19 +1000

    Introducing the MRI project

--- assets/assets.js
+++ assets/assets.js
@@ -99,7 +99,7 @@ export const assets = {
 export const workData = [
     {
         slug: 'mri-classification',
-        title: 'MRI Classification Using CNNs',
+        title: 'CNNs - Brain Cancer MRI Classification',
         description: 'Machine Learning',
         bgImage: '/projectImages/mri-CNN/work-1-MRI.png',
         headerImage: '/projectImages/mri-CNN/header_mri_banner.png',
