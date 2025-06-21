58e7381d19a428a3df692bcfb749a1f98e65c0db
Samuel David Peterson <s3722250@student.rmit.edu.au>
2025-06-21 12:48:23 +1000

    moved css to styles folder

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Outfit, Ovo } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 import { ThemeProvider } from "./context/ThemeContext"; 
 
 const outfit = Outfit({
