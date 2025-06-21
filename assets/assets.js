647ed4de7bab5a16522c6b302be8d6009e8280cf
Samuel David Peterson <s3722250@student.rmit.edu.au>
2025-06-21 16:24:02 +1000

    commenting out project placeholders.

--- assets/assets.js
+++ assets/assets.js
@@ -106,24 +106,24 @@ export const workData = [
         contentPath: 'mri-classification.mdx', 
         tags: ['TensorFlow', 'Medical Imaging', 'Python'],
     },
-    {
-        slug: 'geo-based-app',
-        title: 'Geo based app',
-        description: 'Mobile App',
-        bgImage: '/work-2.png',
-    },
-    {
-        slug: 'photography-site',
-        title: 'Photography site',
-        description: 'Web Design',
-        bgImage: '/work-3.png',
-    },
-    {
-        slug: 'ui-ux-designing',
-        title: 'UI/UX designing',
-        description: 'UI/UX Design',
-        bgImage: '/work-4.png',
-    },
+    // {
+    //     slug: 'geo-based-app',
+    //     title: 'Geo based app',
+    //     description: 'Mobile App',
+    //     bgImage: '/work-2.png',
+    // },
+    // {
+    //     slug: 'photography-site',
+    //     title: 'Photography site',
+    //     description: 'Web Design',
+    //     bgImage: '/work-3.png',
+    // },
+    // {
+    //     slug: 'ui-ux-designing',
+    //     title: 'UI/UX designing',
+    //     description: 'UI/UX Design',
+    //     bgImage: '/work-4.png',
+    // },
 ]
 
 export const serviceData = [
