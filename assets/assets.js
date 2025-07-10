257c76ac29be73c39ff2ed1dded7122d2d307526
Samuel David Peterson <s3722250@student.rmit.edu.au>
2025-07-10 13:16:21 +1000

    updated about me section

--- assets/assets.js
+++ assets/assets.js
@@ -134,9 +134,9 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Pyton, HTML, CSS, JavaScript, React Js, Next Js, SQL' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Python, HTML, CSS, JavaScript, React Js, Next Js, SQL' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Sci Applied Science M.Sc Data Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Applying interest and continuous learning through the development of personal projects' }
 ];
 
 export const toolsData = [
